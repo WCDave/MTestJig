d6cdf34da36343f274ba332df1ef806b6832e948
WCDave <david.butterwick@gmail.com>
2017-06-02 15:10:30 -0400

    fixing junits

--- SolarSystem/src/test/java/orbitService/OrbitCalcImpl2Test.java
+++ SolarSystem/src/test/java/orbitService/OrbitCalcImpl2Test.java
@@ -4,7 +4,6 @@ import domain.SolarSystemObject;
 import orbitDataDao.IOrbitData;
 import orbitService.impl.OrbitCalcImpl2;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,7 +37,6 @@ public class OrbitCalcImpl2Test {
     }
 
     @Test
-    @Ignore
     public void testComputeTA() throws Exception {
 
 
